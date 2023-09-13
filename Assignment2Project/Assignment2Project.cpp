d0a4b629d42eb7dabed79fcdf56021e1969190af
DavidLewis20 <144903911+DavidLewis20@users.noreply.github.com>
2023-09-13 11:50:17 +0100

    Delete Assignment2Project directory

--- Assignment2Project/Assignment2Project.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Assignment2Project.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Assignment2Project, "Assignment2Project" );
--- Assignment2Project/Assignment2ProjectGameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "Assignment2ProjectGameModeBase.h"
-
